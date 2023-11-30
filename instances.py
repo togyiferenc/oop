0e8b62dceae67692432d80b7b573fb9a6f905bd7
togyiferenc <125687765+togyiferenc@users.noreply.github.com>
2023-11-30 18:57:33 +0100

    Create instances.py

---
+++ instances.py
@@ -0,0 +1,24 @@
+from met import *
+
+Szoba1 = EgyagyasSzoba(10000, 1)
+Szoba2 = EgyagyasSzoba(11000, 2)
+Szoba3 = EgyagyasSzoba(12000, 3)
+Szoba4 = EgyagyasSzoba(13000, 4)
+
+Szoba5 = KetagyasSzoba(15000, 5)
+Szoba6 = KetagyasSzoba(16000, 6)
+Szoba7 = KetagyasSzoba(17000, 7)
+Szoba8 = KetagyasSzoba(18000, 8)
+
+
+Szálloda = Szalloda("Példa Hotel")
+
+
+Szálloda.szoba_hozzaad(Szoba1)
+Szálloda.szoba_hozzaad(Szoba2)
+Szálloda.szoba_hozzaad(Szoba3)
+Szálloda.szoba_hozzaad(Szoba4)
+Szálloda.szoba_hozzaad(Szoba5)
+Szálloda.szoba_hozzaad(Szoba6)
+Szálloda.szoba_hozzaad(Szoba7)
+Szálloda.szoba_hozzaad(Szoba8)
